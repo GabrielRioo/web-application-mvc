c6a1418909fc10cf48e2359ac0d5bbef9ff90916
GabrielRioo <Gabrielpaivario@gmail.com>
2020-09-17 09:07:57 -0300

    Model Product with the informations about the table in the View

--- FN.Store/FN.Store.UI/Controllers/ProdutosController.cs
+++ FN.Store/FN.Store.UI/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FN.Store.UI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,14 @@ namespace FN.Store.UI.Controllers
     {
         public ViewResult Index()
         {
-            return View();
+            var produtos = new List<Produto>() { 
+            new Produto() { Id = 1, Nome = "Carne", Preco = 70.5M, Qtde = 150, Tipo = "Alimento" },
+            new Produto() { Id = 2, Nome = "Pasta de Dente", Preco = 9.5M, Qtde = 30, Tipo = "Higiene" },
+            new Produto() { Id = 3, Nome = "Desinfetante", Preco = 8.99M, Qtde = 90, Tipo = "Limpeza" }
+            
+            };
+
+            return View(produtos);
         }
     }
 }
---
+++ FN.Store/FN.Store.UI/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN.Store.UI.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Tipo { get; set; }
+        public short Qtde { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
