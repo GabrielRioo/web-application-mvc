5847e79d23528f4fadf8c58a8cc4a8f23fb3d494
GabrielRioo <Gabrielpaivario@gmail.com>
2020-09-17 08:50:19 -0300

    Produtos Controller and View with a table

---
+++ FN.Store/FN.Store.UI/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FN.Store.UI.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
