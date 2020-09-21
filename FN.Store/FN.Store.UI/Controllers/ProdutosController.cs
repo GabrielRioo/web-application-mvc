d5a88e079b24c0f131ef3525b5341cfa69a24233
GabrielRioo <Gabrielpaivario@gmail.com>
2020-09-21 08:43:45 -0300

    create data base

--- FN.Store/FN.Store.UI/Controllers/ProdutosController.cs
+++ FN.Store/FN.Store.UI/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
-﻿using FN.Store.UI.Models;
+﻿using FN.Store.UI.Data;
+using FN.Store.UI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,9 @@ namespace FN.Store.UI.Controllers
     {
         public ViewResult Index()
         {
-            var produtos = new List<Produto>() { 
-            new Produto() { Id = 1, Nome = "Carne", Preco = 70.5M, Qtde = 150, Tipo = "Alimento" },
-            new Produto() { Id = 2, Nome = "Pasta de Dente", Preco = 9.5M, Qtde = 30, Tipo = "Higiene" },
-            new Produto() { Id = 3, Nome = "Desinfetante", Preco = 8.99M, Qtde = 90, Tipo = "Limpeza" }
-            
-            };
+            var ctx = new FNStoreDataContext();
+            var produtos = ctx.Produtos.ToList();
+
 
             return View(produtos);
         }
---
+++ FN.Store/FN.Store.UI/Data/DbInitializer.cs
@@ -0,0 +1,22 @@
+﻿using FN.Store.UI.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace FN.Store.UI.Data
+{
+	internal class DbInitializer : CreateDatabaseIfNotExists<FNStoreDataContext>
+	{
+		protected override void Seed(FNStoreDataContext context)
+		{
+			var produtos = new List<Produto>() {
+			new Produto() { Nome = "Carne", Preco = 70.5M, Qtde = 150, Tipo = "Alimento" },
+			new Produto() { Nome = "Pasta de Dente", Preco = 9.5M, Qtde = 30, Tipo = "Higiene" },
+			new Produto() { Nome = "Desinfetante", Preco = 8.99M, Qtde = 90, Tipo = "Limpeza" },
+			new Produto() { Nome = "Celular", Preco = 899.99M, Qtde = 52, Tipo = "Eletronico" }
+
+			};
+			context.Produtos.AddRange(produtos);
+			context.SaveChanges();
+		}
+	}
+}
